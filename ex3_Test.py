3ab59d8a23da407f394a4931ccd14757c298403d
slarap <145837862+slarap@users.noreply.github.com>
2023-10-27 09:52:54 +0200

    Delete ex3_Test.py

--- ex3_Test.py
+++
@@ -1,7 +0,0 @@
-from ex2_class_Point import Point
-from ex3_class_rectangle import Rectangle
-point1 = Point(2, 8)
-point2 = Point(1, 5)
-rectangle = Rectangle(point1, point2)
-print(f'El area del rectángulo es {rectangle.area()}')
-print(f'El perímetro del rectángulo es {rectangle.perimetro()}')
