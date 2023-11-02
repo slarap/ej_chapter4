56cbed0707186a407b7c47357ae0ddec9a19fc65
slarap <145837862+slarap@users.noreply.github.com>
2023-11-02 20:51:02 +0100

    Add files via upload

---
+++ ex5_Test.py
@@ -0,0 +1,15 @@
+from ex5_class_terminal import Terminal
+t1 = Terminal("666112233")
+t2 = Terminal("666744459")
+t3 = Terminal("632128919")
+t4 = Terminal("664135818")
+print(t1)
+print(t2)
+t1.call(t2, 420)
+print(t2)
+t1.call(t3, 210)
+print(t1)
+print(t2)
+print(t3)
+print(t4)
+
---
+++ ex5_class_terminal.py
@@ -0,0 +1,37 @@
+class Terminal:
+    def __init__(self, digits):
+        if digits[0] in ["9", "7", "6"] and len(digits) == 9:
+            self.__digits = digits
+        else:
+            raise TypeError(f'{digits} no es un número de teléfono válido.')
+        self.__time_made = 0
+        self.__time_received = 0
+        self.__total_time = 0
+    @property
+    def time_received(self):
+        return self.__time_received
+    @time_received.setter
+    def time_received(self, call_time):
+        self.__time_received += call_time
+    @property
+    def time_made(self):
+        return self.__time_made
+    @time_made.setter
+    def time_made(self, call_time):
+        self.__time_made += call_time
+
+    @property
+    def total_time(self):
+        return self.time_made + self.time_received
+
+    def call(self, terminal, call_time):
+        self.__time_made += call_time
+        self.__total_time += call_time
+        terminal.time_received += call_time
+        
+
+    def __str__(self):
+        return f'{self.__digits} - Conversation time: {self.__total_time}'
+
+
+
