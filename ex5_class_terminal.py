97a9584fe161ab9d51fbf22e9d32ff45e3ce415d
slarap <145837862+slarap@users.noreply.github.com>
2023-11-03 13:24:29 +0100

    Add files via upload

--- ex5_class_terminal.py
+++ ex5_class_terminal.py
@@ -13,22 +13,20 @@ class Terminal:
     @time_received.setter
     def time_received(self, call_time):
         self.__time_received += call_time
+        self.__total_time += call_time
     @property
     def time_made(self):
         return self.__time_made
     @time_made.setter
     def time_made(self, call_time):
         self.__time_made += call_time
-
+        self.__total_time += call_time
     @property
     def total_time(self):
-        return self.time_made + self.time_received
-
+        return self.__total_time
     def call(self, terminal, call_time):
-        self.__time_made += call_time
-        self.__total_time += call_time
+        self.time_made += call_time
         terminal.time_received += call_time
-        
 
     def __str__(self):
         return f'{self.__digits} - Conversation time: {self.__total_time}'
