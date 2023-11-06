0f34851c451fe1d490e1195cb7797b7ec25f1188
slarap <145837862+slarap@users.noreply.github.com>
2023-11-06 21:21:45 +0100

    Add files via upload

--- ex6_class.py
+++ ex6_class.py
@@ -1,10 +1,31 @@
 class Card:
     def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+    @property
+    def suit(self):
+        return self.__suit
+    @suit.setter
+    def suit(self, suit):
         if suit in ["'Hearts', 'Diamonds', 'Clubs', 'Spades'"]:
             self.__suit = suit
-        if value in range(1,10) or suit in ["J", "Q", "K"]:
+        else:
+            raise TypeError("Suit or value is not valid")
+    @property
+    def value(self):
+        return self.__value
+    @value.setter
+    def value(self, value):
+        if value in range(1,10) or value in ["J", "Q", "K"]:
             self.__value = value
         else:
             raise TypeError("Suit or value is not valid")
-class Hand:
-    
+class Hand():
+    def __init__(self):
+        self.cards=[]
+    @property
+    def cards(self):
+        return self.__cards
+    @cards.setter
+    def cards(self, card):
+        self.cards.append(card)
